9a67baa349059e2be65cb2202416082ed44a285e
sameer-555 <sameerborkar6@gmail.com>
2023-11-21 12:28:25 +0530

    minor fix

--- server.py
+++ server.py
@@ -54,5 +54,5 @@ def get_users():
     return jsonify({'users': users})
 
 
-# if __name__ == '__main__':
-#     app.run(port=8000, debug=True)
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
